1e98eedfa9e5a805d05bb35290d8f9fdd8e14804
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-07 01:54:59 +0530

    initialize Express server and connect to MongoDB - Configure environment variables using 'dotenv' - Use 'mongoose' to connect to MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 import express from "express";
 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 const PORT = 3000;
 
