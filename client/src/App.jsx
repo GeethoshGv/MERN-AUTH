1f94fb8ec7a737ab20dcb305fd789b71254eee23
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-07 00:52:25 +0530

    add React Router and set up basic page structure
    - Integrate React Router for navigation
    - Create Home, About, SignUp, SignIn, and Profile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
+import SignIn from "./pages/signin/SignIn.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+import About from "./pages/about/About.jsx";
+
 const App = () => {
   return (
     <>
-      <section>
-        <h1>hello world</h1>
-      </section>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
