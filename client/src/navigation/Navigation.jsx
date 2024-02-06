3006d3053aed74af024a289d6dcc9519564d9ccd
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-07 01:22:33 +0530

    implement Navigation component with React Router links
    - Integrate React Router for navigation
    - Utilize NavLink components for Sign-In and SignUp with conditional styling

--- client/src/navigation/Navigation.jsx
+++ client/src/navigation/Navigation.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import "./nav.scss";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav>
       <div className="nav-div">
         <div className="title-div">
-          <h1>MERN-AUTH</h1>
+          <Link to="/">
+            <h1>MERN-AUTH</h1>
+          </Link>
         </div>
         <div className="pages-div">
-          <h2>Sign-In</h2>
-          <h2>SignUp</h2>
+          <NavLink
+            to="/sign-in"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            <h1>Sign-In</h1>
+          </NavLink>
+          <NavLink
+            to="/sign-up"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            <h1>SignUp</h1>
+          </NavLink>
         </div>
       </div>
     </nav>
