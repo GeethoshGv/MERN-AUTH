c2bbbd099b5084998aad76eb73d4545b59f03f94
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-07 16:11:34 +0530

    enhance user sign-up endpoint with password confirmation - Check if 'password' and 'confirmPassword' match before processing the sign-up

--- server/controller/authControllers.js
+++ server/controller/authControllers.js
@@ -2,10 +2,18 @@ import User from "../model/User.js";
 import bcryptjs from "bcryptjs";
 
 export const SignUp = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, confirmPassword } = req.body;
 
-  const hashPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashPassword });
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+
+  const hashPassword = bcryptjs.hashSync(password, 10); //hashing the password using bcrypt
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
   try {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
