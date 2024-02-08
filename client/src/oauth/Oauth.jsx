f9128bf0dad60c7d75e2278751b608f289973831
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-08 23:09:02 +0530

    implement Google OAuth in Oauth component - Use 'signInWithPopup' to initiate the Google OAuth popup - Extract user information from the authentication result

--- client/src/oauth/Oauth.jsx
+++ client/src/oauth/Oauth.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import "./oauth.scss";
+import { app } from "../firebase/Firebase";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { useDispatch } from "react-redux";
 
 const Oauth = () => {
+  const dispatch = useDispatch();
+  const handleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider); //result
+
+      const res = await fetch("api/user/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userName: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+          verified: result.user.emailVerified,
+        }),
+      });
+
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+    } catch (error) {
+      console.log("could not login with Google Auth".error);
+    }
+  };
+
   return (
-    <button type="button" className="oauth_button">
+    <button type="button" className="oauth_button" onClick={handleClick}>
       Google
     </button>
   );
