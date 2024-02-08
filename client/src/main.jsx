ebf33ea2313a9515ac19962f58f03281880871b6
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-08 21:17:25 +0530

    integrate redux-persist in root component - Use 'redux-persist' to persist and rehydrate the Redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style/style.scss";
-import { store } from "./redux/Store.jsx";
+import { store, persistor } from "./redux/Store.jsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
