2ebee8746525a3f668c50fb9ccdf95a82b8b076c
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-09 18:44:10 +0530

    updated profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,8 +1,51 @@
 import "./profile.scss";
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../../firebase/Firebase.js";
 
 const Profile = () => {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
   const { currentUser } = useSelector((state) => state.user);
+  const [imageError, setImageError] = useState(false);
+  const [imagePercent, setImagePercent] = useState(0);
+
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
 
   return (
     <div className="profile_div">
@@ -10,9 +53,31 @@ const Profile = () => {
 
       <div className="profile_form_div">
         <form action="">
+          <input
+            type="file"
+            ref={fileRef}
+            hidden
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
           <div className="profile_img_div">
-            <img src={currentUser.profilePicture} alt="picture" />
+            <img
+              src={currentUser.profilePicture}
+              alt="picture"
+              onClick={() => fileRef.current.click()}
+            />
           </div>
+          <p className="text-sm self-center">
+            {imageError ? (
+              <span>Error uploading image (size must be less than 2 MB)</span>
+            ) : imagePercent > 0 && imagePercent < 100 ? (
+              <span>{`Uploading: ${imagePercent} %`}</span>
+            ) : imagePercent === 100 ? (
+              <span>Image uploaded successfully</span>
+            ) : (
+              ""
+            )}
+          </p>
           <input
             defaultValue={currentUser.username}
             type="text"
