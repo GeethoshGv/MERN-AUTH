09b339fe2fb5efd80c5bbee6faf0adc295e9eb39
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-09 13:37:20 +0530

    added profile page and styling

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import "./profile.scss";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>
+    <div className="profile_div">
       <h1>profile</h1>
+
+      <div className="profile_form_div">
+        <form action="">
+          <img src={currentUser.profilePicture} alt="" />
+          <input
+            defaultValue={currentUser.username}
+            type="text"
+            id="username"
+            placeholder="Username"
+          />
+          <input
+            defaultValue={currentUser.email}
+            type="email"
+            id="email"
+            placeholder="Email"
+          />
+          <input type="password" id="password" placeholder="password" />
+          <div className="button_div_profile">
+            <button>Update</button>
+            <button>Sign-Out</button>
+          </div>
+          <span>Delete Account</span>
+        </form>
+      </div>
     </div>
   );
 };
