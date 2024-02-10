b8b1f7868b838adedbb7a92539171261654bddcf
Geethosh G V <133562856+GeethoshGv@users.noreply.github.com>
2024-02-10 13:35:02 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-authgv-guvi.onrender.com",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
