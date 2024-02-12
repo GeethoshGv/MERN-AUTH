b1c922cdb7220e2f88afe1e1cb44ebf2e3a20273
Geethosh G V <133562856+GeethoshGv@users.noreply.github.com>
2024-02-13 00:20:50 +0530

    Update About.jsx

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,14 +1,57 @@
+
 import React from "react";
 import "./about.scss";
 
 const About = () => {
   return (
     <div className="about_div ">
-      <div>
-        <h1>About</h1>
+      <h1 className="tech_stack">Features</h1>
+
+      <div className="tech">
+        <div>
+          <h1>Firebase Authentication </h1>
+          <p>Enable seamless user access with Google Sign-In.</p>
+          <p>
+            Empower users to effortlessly log in using their Google credentials{" "}
+          </p>
+        </div>
+
+        <div>
+          <h1> Redux Persist</h1>
+          <p>
+            Preserve user authentication state persistently across app sessions
+          </p>
+          <p>
+            Maintaining authentication status, even when users close and reopen
+            your app.{" "}
+          </p>
+        </div>
+
+        <div>
+          <h1>FireBase</h1>
+          <p>
+            Using Bcrypt for robust password hashing, safeguarding user
+            credentials against unauthorized access.
+          </p>
+        </div>
+        <div>
+          <h1>JSON Web Token </h1>
+          <p>
+            Securely transmit user information between client and server using
+            compact, URL-safe tokens
+          </p>
+        </div>
+        <div>
+          <h1>Others </h1>
+          <p>
+            User can update there password , username , profile ,age , DOB ,
+            gender,phone number
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default About;
+
